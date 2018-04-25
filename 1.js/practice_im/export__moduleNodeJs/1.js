f59c9a088d52950ffa4930be6a134511b870ca0e
UbuntuAsus <phduc.phanhongduc@gmail.com>
2018-04-25 15:48:29 +0300

    25.4.2018

---
+++ 1.js/practice_im/export__moduleNodeJs/1.js
@@ -0,0 +1,4 @@
+const a = 5;
+ let b = ((a)=>{ console.log (a)});
+
+ export { a,b}
---
+++ asking.js
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+    <head>
+    <title>Funny</title>
+
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+
+    </head>
+
+    <body>
+    <header class="header">
+    <div class="view-range">
+    <div class="content-header header__logo-name-company">
+    <div class="header__logo">
+    <i class="fas fa-link header__icon"></i>
+    </div>
+    <h3 class="header__name-company">Lord Software</h3>
+</div>
+</div>
+</header>
+
+<div class="list">
+    <div class="view-range">
+    <h3 class="list__name">List of particpants</h3>
+<div class="list__infos">
+    <div class="list__row list__row--adding">
+    <div class="content-input list__input-btn">
+    <div class="input">
+    <input type="text" class="input__adding" value="Full name">
+    <input type="email" class="input__adding input__adding--mail" value="E-mail address">
+    <input type="tel" class="input__adding" value="Phone number">
+    </div>
+    <button class="btn btn--add">Add new</button>
+</div>
+</div>
+<div class="list__infos">
+    <table class="table">
+    <thead>
+    <tr>
+    <th>
+    <div class="list__t-name-icon">
+    <h3 class="title title--name">Name</h3>
+    <i class="fas fa-arrow-down list__t-icon"></i>
+    </div>
+    </th>
+    <th>
+    <h3 class="title title--mail">E-mail address</h3>
+</th>
+<th>
+<h3 class="title title--tel">Phone number</h3>
+</th>
+<th></th>
+
+</tr>
+</thead>
+
+<tbody>
+<tr>
+<td>
+<p class="info__text info__text--name">Joe Doe</p>
+</td>
+<td>
+<p class="info__text info__text--mail">john.doe@gmail.com</p>
+</td>
+<td>
+<p class="info__text info__text--tel">0405678471</p>
+</td>
+<td>
+<div class="list__cover-icons">
+    <i class="fas fa-pencil-alt info__icon"></i>
+    <i class="fas fa-trash info__icon"></i>
+    </div>
+    </td>
+    </tr>
+    <tr>
+    <td>
+    <p class="info__text info__text--name">George Clooney</p>
+</td>
+<td>
+<p class="info__text info__text--mail">george.clooney@hollywood.com</p>
+</td>
+<td>
+<p class="info__text info__text--tel">0504629898</p>
+</td>
+<td>
+<div class="list__cover-icons">
+    <i class="fas fa-pencil-alt info__icon"></i>
+    <i class="fas fa-trash info__icon"></i>
+    </div>
+    </td>
+    </tr>
+    <tr>
+    <td>
+    <p class="info__text info__text--name"> Clooney</p>
+    </td>
+    <td>
+    <p class="info__text info__text--mail">george.clooney@hollywood.com</p>
+</td>
+<td>
+<p class="info__text info__text--tel">0504629898</p>
+</td>
+<td>
+<div class="list__cover-icons">
+    <i class="fas fa-pencil-alt info__icon"></i>
+    <i class="fas fa-trash info__icon"></i>
+    </div>
+    </td>
+    </tr>
+    <tr>
+    <td>
+    <p class="info__text info__text--name"> Clooney</p>
+    </td>
+    <td>
+    <p class="info__text info__text--mail">george.clooney@hollywood.com</p>
+</td>
+<td>
+<p class="info__text info__text--tel">0504629898</p>
+</td>
+<td>
+<div class="list__cover-icons">
+    <i class="fas fa-pencil-alt info__icon"></i>
+    <i class="fas fa-trash info__icon"></i>
+    </div>
+    </td>
+    </tr>
+    </tbody>
+    </table>
+    </div>
+
+    <!-- <div class="list__row list__row--infos">
+    <div class="content-info list__title">
+    <div class="list__t-name-icon">
+    <h3 class="title title--name">Name</h3>
+    <i class="fas fa-arrow-down list__t-icon"></i>
+    </div>
+    <h3 class="title title--mail">E-mail address</h3>
+<h3 class="title title--tel">Phone number</h3>
+</div>
+</div> -->
+
+
+<!-- <div class="list__detail">
+    <div class="list__row list__row--infos">
+    <div class="content-info info">
+    <p class="info__text info__text--name">Joe Doe</p>
+<p class="info__text info__text--mail">john.doe@gmail.com</p>
+<p class="info__text info__text--tel">0405678471</p>
+<div class="list__cover-icons">
+    <i class="fas fa-pencil-alt info__icon"></i>
+    <i class="fas fa-trash info__icon"></i>
+    </div>
+
+    </div>
+    </div>
+
+    <div class="list__row list__row--infos">
+    <div class="content-info info">
+    <p class="info__text info__text--name">George Clooney</p>
+<p class="info__text info__text--mail">george.clooney@hollywood.com</p>
+<p class="info__text info__text--tel">0504629898</p>
+<div class="list__cover-icons">
+    <i class="fas fa-pencil-alt info__icon"></i>
+    <i class="fas fa-trash info__icon"></i>
+    </div>
+    </div>
+    </div>
+
+    <div class="list__row list__row--infos">
+    <div class="content-info info">
+    <p class="info__text info__text--name">George Clooney</p>
+<p class="info__text info__text--mail">george.clooney@hollywood.com</p>
+<p class="info__text info__text--tel">0504629898</p>
+<div class="list__cover-icons">
+    <i class="fas fa-pencil-alt info__icon"></i>
+    <i class="fas fa-trash info__icon"></i>
+    </div>
+    </div>
+    </div>
+
+    <div class="list__row list__row--infos">
+    <div class="content-info info">
+    <p class="info__text info__text--name">George Clooney</p>
+<p class="info__text info__text--mail">george.clooney@hollywood.com</p>
+<p class="info__text info__text--tel">0504629898</p>
+<div class="list__cover-icons">
+    <div class="info__icon">
+    <i class="fas fa-pencil-alt"></i>
+    </div>
+
+    <div class="info__icon">
+    <i class="fas fa-trash"></i>
+    </div>
+
+    </div>
+
+    </div>
+    </div>
+    </div> -->
+
+    <div class="list__row list__row--infos">
+    <div class="content-input list__edit-save">
+    <div class="input">
+    <input type="text" class="input__editing" value="George Clooney">
+    <input type="email" class="input__editing input__editing--mail" value="george.clooney@hollywood.com">
+    <input type="tel" class="input__editing" value="0504629898">
+    </div>
+    <div class="list__btn">
+    <button class="btn btn--cancel">Cancel</button>
+    <button class="btn btn--save">Save</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+    <div id="list">
+    <ul>
+    <li class="list-item" data-animal-code="dog" data-id="1">salsla</li>
+    <li class="list-item">Cat</li>
+    <li class="list-item">Rat</li>
+    </ul>
+    </div>
+    </div>
+
+    <script>
+    let workers= [
+    {
+        id: 'a123456aa1',
+        name: 'DaveGreat',
+        email: 'dave@gmail.com',
+        tel: '123456789'
+    },
+    {
+        id: 'aaaa1',
+        name: 'beckham',
+        email: 'davidbacks@gmail.com',
+        tel: '6598741'
+    },
+];
+
+let programe1 = (function () {
+
+    let className = document.getElementsByClassName("list");
+
+    function removeTextInput(a) {
+        if (a.target.tagName === 'input' || a.target.tagName.toLowerCase() === 'input') {
+            a.target.value = ' '
+        }
+    }
+
+    function preventLoadPage(a) {
+        console.log(a);
+    }
+
+    function doSomethingPrivate(a) {
+        console.log(a)
+    }
+
+    return {
+
+        init : function()  {
+            className[0].addEventListener("click", function (a) {
+
+                //this.removeTextInput_pub(e);
+                apply(programe1).preventLoadPage_pub(5)
+                //console.log('demo123');
+
+            });
+        },
+
+
+    };
+
+})();
+
+
+programe1.init();
+
+</script>
+</body>
+
+</html>
